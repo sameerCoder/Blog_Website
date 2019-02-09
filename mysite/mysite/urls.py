a491bd85e0c1501ce754b579a9107a10a1766e66
sameerCoder <chatwithnubia2@gmail.com>
2019-02-09 05:52:46 +0530

    first commit

---
+++ mysite/mysite/urls.py
@@ -0,0 +1,29 @@
+"""mysite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url, include
+from django.contrib import admin
+
+from django.contrib.auth import views
+#from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'', include('blog.urls')),
+   url(r'^accounts/login/$', views.login, name='login'),
+  #  url(r'^login/$', auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
+    url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
+    #url(r'^login/$', auth_views.LogoutView.as_view(template_name="useraccounts/login.html"), name="login", kwargs={'next_page': '/'}),
+]
